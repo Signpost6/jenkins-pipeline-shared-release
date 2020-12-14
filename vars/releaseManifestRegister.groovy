b8edd8238bea0cc0a8227df6b901a5c908c15af7
Victor Cetinic <vcetinick@signpost6.com.au>
2020-12-14 22:42:57 +0100

    Integrted release maifest

--- vars/releaseManifestRegister.groovy
+++ vars/releaseManifestRegister.groovy
@@ -1,3 +1,6 @@
+import com.signpost6.ReadManifestOptions
+import com.signpost6.ReleaseManifestReader
+
 /**
  * Determines the current maven version of the project
  *
@@ -17,5 +20,8 @@ def call(body) {
         body.delegate = config
         body()
     }
+
+    ReleaseManifestReader.read(
+            new ReadManifestOptions(gitUrl: 'git@github.com:Signpost6/jenkins-example-java-project.git'))
 
 }
